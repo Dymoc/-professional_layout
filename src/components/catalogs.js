62742d0aed66cb827d29263a21fa04fbe3b08cec
dymoc <dymoc@mail.ru>
2020-10-27 08:55:23 +0300

    изменено время задержки заполнения

--- src/components/catalogs.js
+++ src/components/catalogs.js
@@ -10,7 +10,7 @@ let catalogOfIndex = {
         setTimeout(() => {
             this.button = document.querySelectorAll('.add_to_cart');
             this._handleActionsButtonAddToCart();
-        }, 350);
+        }, 400);
     },
     _fillCatalog() {
         fetch(this.urlBD)
@@ -50,7 +50,7 @@ let catalogOfCatalog = {
         setTimeout(() => {
             this.button = document.querySelectorAll('.add_to_cart');
             this._handleActionsButtonAddToCart();
-        }, 350);
+        }, 400);
     },
     _fillCatalog() {
         fetch(this.urlBD)
